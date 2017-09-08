74ac370a4d12794da6668e73e4c5cc698835646e
Simon Charette <charette.s@gmail.com>
2017-09-07 20:48:27 -0400

    Bumped version number to 1.4.6.

--- polymodels/__init__.py
+++ polymodels/__init__.py
@@ -2,6 +2,6 @@ from __future__ import unicode_literals
 
 from django.utils.version import get_version
 
-VERSION = (1, 4, 6, 'alpha', 3)
+VERSION = (1, 4, 6, 'final', 0)
 
 __version__ = get_version(VERSION)

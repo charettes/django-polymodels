0d273172fb769f8b28928473ccfddc46d736d8dd
Filip Weidemann <filip.weidemann@outlook.de>
2023-04-25 16:46:07 +0200

    Added tested support for Django 4.2
    Also removed support for Python 3.6 an unsupported Django versions.

--- tests/base.py
+++ tests/base.py
@@ -1,3 +1,4 @@
+from django import VERSION as DJANGO_VERSION
 from django.contrib.contenttypes.models import ContentType
 from django.test.testcases import TestCase
 
@@ -5,6 +6,15 @@ from polymodels.models import BasePolymorphicModel
 
 
 class TestCase(TestCase):
+    DJANGO_GTE_42 = (4, 2) <= DJANGO_VERSION
+
+    def assertQuerysetEqual(self, qs, values, transform=None, ordered=True, msg=None):
+        if self.DJANGO_GTE_42:
+            super().assertQuerySetEqual(qs, values, transform=transform or repr, ordered=ordered, msg=msg)
+        else:
+            super().assertQuerysetEqual(qs, values, transform=transform or repr, ordered=ordered, msg=msg)
+        
+
     def tearDown(self):
         ContentType.objects.clear_cache()
         BasePolymorphicModel.subclass_accessors.clear()

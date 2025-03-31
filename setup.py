58284dfcc3cf35b6239edf2953c7265724fd7ad3
Simon Charette <simon.charette@zapier.com>
2025-03-30 22:12:08 -0400

    Drop support for Django < 4.2 and Python < 3.9.

--- setup.py
+++ setup.py
@@ -3,40 +3,39 @@ from setuptools import find_packages, setup
 
 from polymodels import __version__
 
-github_url = 'https://github.com/charettes/django-polymodels'
-long_desc = open('README.rst').read()
+github_url = "https://github.com/charettes/django-polymodels"
+long_desc = open("README.rst").read()
 
 setup(
-    name='django-polymodels',
+    name="django-polymodels",
     version=__version__,
-    description='Polymorphic models implementation for django',
+    description="Polymorphic models implementation for django",
     long_description=long_desc,
-    long_description_content_type='text/x-rst',
+    long_description_content_type="text/x-rst",
     url=github_url,
-    author='Simon Charette',
-    author_email='charette.s@gmail.com',
-    install_requires=(
-        'Django>=3.2',
-    ),
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    author="Simon Charette",
+    author_email="charette.s@gmail.com",
+    install_requires=("Django>=4.2",),
+    packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
-    license='MIT License',
+    license="MIT License",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 3.2',
-        'Framework :: Django :: 4.0',
-        'Framework :: Django :: 4.1',
-        'Framework :: Django :: 4.2',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
+        "Framework :: Django :: 5.1",
+        "Framework :: Django :: 5.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
